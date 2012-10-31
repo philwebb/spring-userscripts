d2ed4ea8e0225a96e0cd3787905e3dbf00ea8f78
Phillip Webb <pwebb@vmware.com>
2012-10-31 10:58:39 -0700

    Support merge without creating remote

--- github-manual-merge.user.js
+++ github-manual-merge.user.js
@@ -19,6 +19,56 @@ function selectText(e) {
     }
 }
 
+function toggleFetchStyle(e) {
+	var fetchstyle = localStorage['github-manual-merge-fetchstyle'] || 'default';
+	fetchstyle = (fetchstyle === 'default' ? 'small' : 'default');
+	localStorage['github-manual-merge-fetchstyle'] = fetchstyle;
+	updateMergeDivContent();
+}
+
+function updateMergeDivContent() {
+
+	var fetchstyle = localStorage['github-manual-merge-fetchstyle'] || 'default';
+
+	var mergeInfo = new Array();
+	if(fetchstyle === 'default') {
+	mergeInfo.push(
+		'git remote add ' + username + ' https://github.com/' + username + '/' + repository + '.git\n');
+		mergeInfo.push(
+			'git fetch ' + username + '\n'+
+			'git checkout --track ' + username + '/' + branch + ' -b ' + branch + "\n");
+	} else {
+		mergeInfo.push(
+			'git fetch https://github.com/' + username + '/' + repository + '.git ' + branch + '\n' +
+			'git checkout -b ' + branch + ' FETCH_HEAD\n');
+	}
+	mergeInfo.push(
+		'git rebase master\n');
+	mergeInfo.push(
+		'git checkout master\n');
+	mergeInfo.push(
+		'git merge --no-ff --log -m "Merge pull request #' + requestnumber + ' from ' + username + '/' + branch + '" ' + branch + "\n");
+
+	mergeDiv.innerHTML = "";
+	var toggleFetchStyleDiv = document.createElement('div');
+	toggleFetchStyleDiv.setAttribute("style", "float: right; cursor: pointer; cursor: hand;");
+	toggleFetchStyleDiv.addEventListener("click", toggleFetchStyle, false);
+	toggleFetchStyleDiv.innerHTML = '<span class="mini-icon mini-icon-refresh"> </span>';
+	mergeDiv.appendChild(toggleFetchStyleDiv);
+
+	for (var i = 0; i < mergeInfo.length; i++) {
+		var mergeInfoElement = document.createElement('pre');
+		mergeDiv.appendChild(mergeInfoElement);
+		if(i < mergeInfo.length-1) {
+			mergeInfoElement.setAttribute("style", "margin-bottom: 6px;");
+		}
+		mergeInfoElement.innerHTML = mergeInfo[i];
+		mergeInfoElement.addEventListener("click", selectText, false);
+	}
+}
+
+// Grab variables we need from the page
+
 var pullHeaderElement = document.getElementById("pull-head")
 var metas = document.getElementsByTagName('meta'); 
 
@@ -39,29 +89,10 @@ var requestnumber= document.evaluate("div/div/span[2]",
 						pullHeaderElement, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
 						.singleNodeValue.textContent.trim().substring(1);
 
-var mergeInfo = new Array();
-mergeInfo.push(
-	'git remote add ' + username + ' https://github.com/' + username + '/' + repository + '.git\n');
-mergeInfo.push(
-	'git fetch ' + username + '\n'+
-	'git checkout --track ' + username + '/' + branch + ' -b ' + branch + '\n');
-mergeInfo.push(
-	'git rebase master');
-mergeInfo.push(
-	'git checkout master');
-mergeInfo.push(
-	'git merge --no-ff --log -m "Merge pull request #' + requestnumber + ' from ' + username + '/' + branch + '" ' + branch);
+// Insert our new div element
 
 var mergeDiv = document.createElement('div');
 mergeDiv.setAttribute("class", "pull-head");
 mergeDiv.setAttribute("style", "padding: 10px;");
-for (var i = 0; i < mergeInfo.length; i++) {
-	var mergeInfoElement = document.createElement('pre');
-	mergeDiv.appendChild(mergeInfoElement);
-	if(i < mergeInfo.length-1) {
-		mergeInfoElement.setAttribute("style", "margin-bottom: 6px;");
-	}
-	mergeInfoElement.innerHTML = mergeInfo[i];
-	mergeInfoElement.addEventListener("click", selectText, false);
-}
 pullHeaderElement.parentNode.insertBefore(mergeDiv, pullHeaderElement.nextSibling);
+updateMergeDivContent();
