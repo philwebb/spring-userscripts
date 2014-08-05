43b2b083b3fc15fdf45b6bad756edbe565314bc4
Phillip Webb <pwebb@gopivotal.com>
2014-08-05 10:52:11 -0700

    Add disable ajax script

---
+++ github-disable-ajax-pulls.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        github-disable-ajax-pulls
+// @namespace   http://springsource.org
+// @description Adds manual merge commands to pull requests
+// @include     https://github.com/*/*/pulls
+// @include     http://github.com/*/*/pulls
+// @version     1
+// @grant		none
+// ==/UserScript==
+
+$( document ).ready(function() {
+	$( document ).off( "click", "" );
+});
