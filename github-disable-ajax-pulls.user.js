ed83945f46cee64371fd7ee2889226eaad3b8631
Phillip Webb <pwebb@pivotal.io>
2014-08-14 22:03:20 -0700

    Only disable issue links
    Allows other features to work.

--- github-disable-ajax-pulls.user.js
+++ github-disable-ajax-pulls.user.js
@@ -2,12 +2,13 @@
 // @name        github-disable-ajax-pulls
 // @namespace   http://springsource.org
 // @description Adds manual merge commands to pull requests
-// @include     https://github.com/*/*/pulls
-// @include     http://github.com/*/*/pulls
+// @include     https://github.com/*/*/pulls*
+// @include     http://github.com/*/*/pulls*
 // @version     1
 // @grant		none
 // ==/UserScript==
 
-$( document ).ready(function() {
-	$( document ).off( "click", "" );
+$('#issues-container a').click(function() {
+	window.location.href = $(this).attr('href');
+	return false;
 });
