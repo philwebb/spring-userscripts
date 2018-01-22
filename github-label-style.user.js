e222b30f025ba489ebf3b4ca1449a2aa79467869
Phillip Webb <pwebb@pivotal.io>
2018-01-21 18:58:21 -0800

    Add label style fix

---
+++ github-label-style.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        github-label-style
+// @namespace   http://springsource.org
+// @description Adds manual merge commands to pull requests
+// @include     https://github.com/*/*
+// @include     http://github.com/*/*
+// @version     1
+// @grant		none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function addGlobalStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
+    }
+
+    addGlobalStyle('.label { line-height: 1.4 !important; font-size: 11px !important; }');
+    addGlobalStyle('.css-truncate-target { max-width: 180px !important; }');
+})();
