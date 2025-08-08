0e14a819065aedc97b27b80290f1328e20221176
Phillip Webb <phil.webb@orbweaver.co.uk>
2025-08-08 11:14:12 +0100

    Update following github.com changes

--- github-sso-dance.js
+++ github-sso-dance.js
@@ -12,43 +12,31 @@
     'use strict'
 
     function githubSignOnAll(rootElement, ssoContinue) {
-        const ssoSectionElement = rootElement?.querySelector("section[aria-labelledby='single-sign-on']")
-        const ssoActionMenuElement = ssoSectionElement?.querySelector("action-menu")
-        const focusGroupElement = ssoActionMenuElement?.querySelector("focus-group")
-        const ssoActionListElement = ssoActionMenuElement?.querySelector("action-list")
-        const ssoAnchorElements = getSsoAnchorElements(ssoSectionElement, ssoActionListElement, ssoContinue)
-        if (ssoAnchorElements && focusGroupElement) {
+        const globalSsoBannerElement = rootElement?.querySelector("react-partial[partial-name='global-sso-banner']")
+        const embeddedDataScriptElement = globalSsoBannerElement?.querySelector("script[data-target='react-partial.embeddedData']")
+        if (embeddedDataScriptElement) {
+            const embeddedData = JSON.parse(embeddedDataScriptElement.textContent)
+            const ssoOrgs = embeddedData?.props?.ssoOrgs
             var signOnAllButtonElement = document.getElementById("github-sso-sign-on-all")
             if (!signOnAllButtonElement) {
                 signOnAllButtonElement = document.createElement('button')
                 signOnAllButtonElement.setAttribute("id", "github-sso-sign-on-all")
                 signOnAllButtonElement.setAttribute("class", "Button--secondary Button--small Button")
+                signOnAllButtonElement.setAttribute("style", "margin: 10px;")
                 signOnAllButtonElement.innerHTML = "Sign-on all"
             }
-            focusGroupElement.prepend(signOnAllButtonElement)
-            signOnAllButtonElement.onclick = () => performGitHubSignOn(ssoAnchorElements)
-        }
-        if (ssoAnchorElements && ssoContinue) {
-            performGitHubSignOn(ssoAnchorElements)
+            globalSsoBannerElement.prepend(signOnAllButtonElement)
+            signOnAllButtonElement.onclick = () => performGitHubSignOn(ssoOrgs)
+            if (ssoContinue && ssoOrgs && ssoOrgs.length > 0) {
+                performGitHubSignOn(ssoOrgs)
+            }
         }
     }
 
-    function getSsoAnchorElements(ssoSectionElement, ssoActionListElement, ssoContinue) {
-        if (ssoActionListElement) {
-            return ssoActionListElement?.querySelectorAll("li.ActionListItem a")
-        }
-        if (ssoContinue) {
-            const bannerActionsDivElement = ssoSectionElement?.querySelector("div.flash-action")
-            return bannerActionsDivElement?.querySelectorAll("a.Button")
-        }
-        return null
-    }
-
-    function performGitHubSignOn(ssoAnchorElements) {
-        const href = ssoAnchorElements[0]?.href
-        if (href) {
-            window.location.href = `${href}&sso-continue=true&sso-count=${ssoAnchorElements.length}`
-        }
+    function performGitHubSignOn(ssoOrgs) {
+        const login = ssoOrgs[0]?.login
+        const href = `https://github.com/orgs/${login}/sso?return_to=%2Fnotifications&sso-continue=true&sso-count=${ssoOrgs.length}`
+        window.location.href = new URL(href)
     }
 
     function gitHubSsoClickContinue(params) {
@@ -59,12 +47,12 @@
             const url = new URL(action)
             const returnTo = url.searchParams.get("return_to")
             if (returnTo) {
-                const returnToUrl = new URL(returnTo)
+                const returnToUrl = new URL(`https://github.com/${returnTo}`)
                 returnToUrl.searchParams.delete("sso-continue")
                 if (ssoCount > 1) {
                     returnToUrl.searchParams.set("sso-continue", "true")
                 }
-                url.searchParams.set("return_to", returnToUrl)
+                url.searchParams.set("return_to", returnToUrl.toString().replaceAll("https://github.com/", ""))
                 formElement.action = url
             }
             formElement.submit()
